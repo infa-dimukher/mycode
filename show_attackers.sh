7d3af1fb3d8ea14873f483e19f34b0889d2e34d6
infa-dimukher <shubho1993@gmail.com>
2020-09-09 16:41:26 +0530

    Adding the sample file

--- show_attackers.sh
+++ show_attackers.sh
@@ -2,12 +2,11 @@
 cd /home/shubho/Documents/shellclass/exercises
 grep "rhost" syslog-sample | awk -F 'rhost=' '{print $2}' | cut -d ' ' -f 1 | sort -n | uniq -c | sort -t ' ' -k1 -n > test
 
-while IFS= read -r line
+while read line
 do
-    index= `echo $line | awk '{print $1}'`
-    #if [[ "${index}" > 10 ]]
-    #then
+    num= $line | awk '{print $1}'
+    if [[ $num > 10 ]]
+    then
         echo $line
-        echo $index
-    #fi
+    fi
 done < test
