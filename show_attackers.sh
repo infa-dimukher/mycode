c4542c7a9133bd3325451babbd686c92568c2a15
infa-dimukher <shubho1993@gmail.com>
2020-09-10 23:38:03 +0530

    Final code for show attacker

--- show_attackers.sh
+++ show_attackers.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd /home/shubho/Documents/shellclass/exercises
-grep "rhost" syslog-sample | awk -F 'rhost=' '{print $2}' | cut -d ' ' -f 1 | sort -n | uniq -c | sort -t ' ' -k1 -n > test
-
-while read line
+sample=$(find $HOME -name syslog* | head -n 1)
+#sample_dir=$(dirname ${sample})
+echo -e "Number of attempts\tIP Address\n\n"
+grep "rhost" $sample | awk -F 'rhost=' '{print $2}' | cut -d ' ' -f 1 | sort | uniq -c | sort -nr | while read count ip
 do
-    num= $line | awk '{print $1}'
-    if [[ $num > 10 ]]
+    if [[ "${count}" -gt 10 ]]
     then
-        echo $line
+        echo -e "${count}\t\t\t${ip}"
     fi
-done < test
+done
+exit 0
