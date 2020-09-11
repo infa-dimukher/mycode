7cf2db8f12ecbb6faea7eef02b4abc09812d7ce5
infa-dimukher <shubho1993@gmail.com>
2020-09-12 00:38:25 +0530

    update sftp.sh

--- sftp.sh
+++ sftp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+target_dir=/monitoring/cre
+[ ! -d "${target_dir}" ] && sudo mkdir -p ${target_dir}
 expect << 'EOS'
 spawn sftp tsftp@tsftp.informatica.com:/Home/tsftp/vignesh/final_scripts
 expect "Password:"
--- test.sh
+++ test.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-user_name=$(id -un)
-echo "Your User ID is " ${UID}
-echo "Your username is:  ${user_name}"
-if [[ "$UID" -eq 0 ]]
-then
-    echo "You are root user"
-else
-    echo "You are NOT a root user"
-fi
-#test -f /home/shubho/Documents/test.sh && echo "Its a file and not a directory"
-echo "${#}"
-exit 0
+[ -d "/home/shubho/Documents/mycode/lala" ] && echo "Directory exists."
