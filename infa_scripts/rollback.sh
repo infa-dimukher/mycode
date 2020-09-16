ae5b4523736185967035e24a0f1b9a9677815280
infa-dimukher <dimukherjee@informatica.com>
2020-09-16 21:04:37 +0530

    added rollback script

--- infa_scripts/rollback.sh
+++ infa_scripts/rollback.sh
@@ -5,9 +5,9 @@ cat elb_list.txt | grep int > internal_lbs
 cat elb_list.txt | grep -v int > external_lbs
 for elb in $(cat elb_list.txt)
 do
-  aws elb configure-health-check --load-balancer-name $elb --health-check Target=HTTPS:8443/cmx/,Interval=10,UnhealthyThreshold=2,HealthyThreshold=10,Timeout=5
+  aws elb configure-health-check --load-balancer-name $elb --health-check Target=TCP:8443,Interval=30,UnhealthyThreshold=2,HealthyThreshold=10,Timeout=5
   aws elb modify-load-balancer-attributes --load-balancer-name $elb --load-balancer-attributes "{\"ConnectionSettings\":{\"IdleTimeout\":1800}}"
-  aws elb delete-load-balancer-listeners --load-balancer-name $elb --load-balancer-ports 80
+  aws elb create-load-balancer-listeners --load-balancer-name $elb --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80"
 done
 for int_elb in $(cat internal_lbs)
 do
