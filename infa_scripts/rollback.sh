2ea37394e69a0e6323cf3008ee3942933fcdbd20
infa-dimukher <dimukherjee@informatica.com>
2020-09-16 21:44:42 +0530

    updated rollback script

--- infa_scripts/rollback.sh
+++ infa_scripts/rollback.sh
@@ -9,16 +9,6 @@ do
   aws elb modify-load-balancer-attributes --load-balancer-name $elb --load-balancer-attributes "{\"ConnectionSettings\":{\"IdleTimeout\":1800}}"
   aws elb create-load-balancer-listeners --load-balancer-name $elb --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80"
 done
-for int_elb in $(cat internal_lbs)
-do
-  aws elb create-app-cookie-stickiness-policy --load-balancer-name $int_elb --policy-name ${private_policy_name} --cookie-name JSESSIONID
-  aws elb set-load-balancer-policies-of-listener --load-balancer-name $int_elb --load-balancer-port 443 --policy-names ${private_policy_name}
-done
-for ext_elb in $(cat external_lbs)
-do
-  aws elb create-app-cookie-stickiness-policy --load-balancer-name $ext_elb --policy-name ${public_policy_name} --cookie-name JSESSIONID
-  aws elb set-load-balancer-policies-of-listener --load-balancer-name $ext_elb --load-balancer-port 443 --policy-names ${public_policy_name}
-done
 rm -f internal_lbs
 rm -f external_lbs
 exit 0
