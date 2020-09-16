d46900b9308905ae9f811e2c55d3aeff898b2fef
infa-dimukher <dimukherjee@informatica.com>
2020-09-16 15:56:39 +0530

    updated populate elb list generation script

--- infa_scripts/populate_elb.sh
+++ infa_scripts/populate_elb.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 : > elb_list.txt
-env=$1
-aws ec2 describe-instances --filter ""Name=tag:Name,Values=MDM-*-${env}*"" --query Reservations[].Instances[].Tags | grep -A1 -B1 Name | grep Value | grep -v -e '-ASG-' | awk -F ':' '{print $2}' | sed 's/"//g' | sed 's/,//g' | sed 's/ //g' | awk -F '-' '{print $2}' | sort -u  | grep -iv bastion > customer_list.txt
-
-for cust in $(cat customer_list.txt)
+aws_region="us-east-1 us-west-2 ca-central-1 eu-west-1"
+for region in $aws_region
 do
-  echo "mdm-${cust}-${env}" >> elb_list.txt
-  echo "mdm-int-${cust}-${env}" >> elb_list.txt
+  echo "# ${region} MDM Load balancers" >> elb_list.txt
+  echo -e "-------------------\n" >> elb_list.txt
+  aws elb describe-load-balancers | grep LoadBalancerName | awk '{print $2}' | grep mdm- | sed 's/,//' | sed 's/"//g' >> elb_list.txt
 done
-#rm -f customer_list.txt
 exit 0
