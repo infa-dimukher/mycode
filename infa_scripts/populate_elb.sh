5cb9c124bcd9029e0ed29d91dec585b9027d6560
infa-dimukher <dimukherjee@informatica.com>
2020-09-16 16:02:15 +0530

    update populate script

--- infa_scripts/populate_elb.sh
+++ infa_scripts/populate_elb.sh
@@ -4,7 +4,8 @@ aws_region="us-east-1 us-west-2 ca-central-1 eu-west-1"
 for region in $aws_region
 do
   echo "# ${region} MDM Load balancers" >> elb_list.txt
-  echo -e "-------------------\n" >> elb_list.txt
-  aws elb describe-load-balancers | grep LoadBalancerName | awk '{print $2}' | grep mdm- | sed 's/,//' | sed 's/"//g' >> elb_list.txt
+  echo -e "---------------------------------------------------" >> elb_list.txt
+  aws elb describe-load-balancers --region ${region} | grep LoadBalancerName | awk '{print $2}' | grep mdm- | sed 's/,//' | sed 's/"//g' >> elb_list.txt
+  echo -e "\n"
 done
 exit 0
