437dc9e785a768df948707e9d43ae694e57d06fd
infa-dimukher <dimukherjee@informatica.com>
2023-09-11 21:54:17 +0530

    updating data sources

--- terraform_practice/main.tf
+++ terraform_practice/main.tf
@@ -1,5 +1,15 @@
+data "aws_ami" "get_ami"{
+  most_recent = true
+  owners = amazon
+
+  filter {
+    name = "name"
+    values = ["myami-*"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = var.ami_id
+  ami           = data.aws_ami.get_ami.id
   instance_type = var.instance_type
   subnet        = var.subnet_id
   count         = var.instance_count
